90d42e8622f4d317c8b91d5c1ace5e22ee4c5ebe
kelly elvis <kellyelvisn@gmail.com>
2025-02-06 13:02:24 +0300

    fixed a javascript syntax error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function submitForm(event){
-event.preventDefault();
-window.location.href = 'about.html';
-return false;
+    event.preventDefault();
+    window.location.href = 'about.html';
+    return false;
 }
 
 document.getElementById('qualificationForm').addEventListener('submit', function(event) {
@@ -22,46 +22,11 @@ document.getElementById('qualificationForm').addEventListener('submit', function
     document.getElementById('result').innerText = result;
 });
 
-
-
-document.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("userForm").addEventListener("submit", function (event) {
         event.preventDefault(); 
-
-        alert("Your details have been collected. Move on to the next page of your choice!");this.reset();
+        alert("Your details have been collected. Move on to the next page of your choice!");
+        this.reset(); 
     });
 });
-function submitForm(event){
-    event.preventDefault();
-    window.location.href = 'about.html';
-    return false;
-    }
-    
-    document.getElementById('qualificationForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const qualification = document.getElementById('qualification').value;
-        const continent = document.getElementById('continent').value;
-        let department = '';
-    
-        if (qualification === 'PhD') {
-            department = 'Advanced Research Department';
-        } else if (qualification === 'Masters') {
-            department = 'Applied Science Department';
-        } else if (qualification === 'Bachelors') {
-            department = 'Undergraduate Science Support Department';
-        }
-    
-        const result = `You have been assigned to the ${department} in ${continent}.`;
-        document.getElementById('result').innerText = result;
-    });
-    
-    
-    document.addEventListener("DOMContentLoaded", function () {
-    
-        document.getElementById("userForm").addEventListener("submit", function (event) {
-            event.preventDefault(); 
-    
-            alert("Your details have been collected. Move on to the next page of your choice!");
-            this.reset(); 
-        });
     
